f4ce53cf9b516c25e59c121e1dc460bf4691c949
Abigael Wambui <abygaelwambuiw@gmail.com>
2024-07-29 16:20:08 +0300

    lambda and transient errors

--- Children'shomemanagement/src/addCategory.java
+++ Children'shomemanagement/src/addCategory.java
@@ -18,14 +18,14 @@ public class addCategory extends javax.swing.JFrame {
     /**
      * Creates new form addStudent
      */
-    Connection conn=null;
-    PreparedStatement st=null;
-    ResultSet rs=null;
+    transient Connection conn=null;
+    transient PreparedStatement st=null;
+   transient ResultSet rs=null;
     
     public addCategory() {
         super("Add Category");
         initComponents();
-        conn=(Connection) databaseConnection.connection();
+        conn=databaseConnection.connection();
     }
 
     /**
@@ -345,25 +345,26 @@ public class addCategory extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(addCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(addCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(addCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(addCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+         try {
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+        java.util.logging.Logger.getLogger(addCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
         //</editor-fold>
         
+    //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new addCategory().setVisible(true);
-            }
-        });
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(() -> new addCategory().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
