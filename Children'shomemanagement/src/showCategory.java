822df76e0d6480c56edc225e52bba326a1e9306b
Abigael Wambui <abygaelwambuiw@gmail.com>
2024-07-29 15:55:29 +0300

    no longer using curly brackets-omitted(showCategory)

--- Children'shomemanagement/src/showCategory.java
+++ Children'shomemanagement/src/showCategory.java
@@ -155,22 +155,24 @@ public final class showCategory extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(showCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(showCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(showCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(showCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+         //</editor-fold>
+         
         //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new showCategory().setVisible(true);
-        });
+       
+       try {
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+        java.util.logging.Logger.getLogger(showCategory.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    java.awt.EventQueue.invokeLater(() -> new showCategory().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
