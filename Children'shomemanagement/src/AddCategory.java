03aa81d3e65bc8755f7bea0774e46172ad475fe7
Abigael Wambui <abygaelwambuiw@gmail.com>
2024-07-29 19:19:55 +0300

    try catch-

--- Children'shomemanagement/src/AddCategory.java
+++ Children'shomemanagement/src/AddCategory.java
@@ -266,41 +266,40 @@ public class AddCategory extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void categoryyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_categoryyActionPerformed
-        // TODO add your handling code here:
     }//GEN-LAST:event_categoryyActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-         try {
+        try {
         String category = categoryy.getText();
-        String status = statuss.getText(); 
-        String name=Name.getText();
+        String status = statuss.getText();
+        String name = Name.getText();
         int area = Integer.parseInt(Area.getText());
 
-        String sql = "INSERT INTO category(category,status,name,areacode) VALUES( ?,?,?,?)";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, category);
-        ps.setString(2, status);
+        String sql = "INSERT INTO category(category, status, name, areacode) VALUES(?, ?, ?, ?)";
         
-        ps.setString(3,name);
-        ps.setInt(4,area);
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, category);
+            ps.setString(2, status);
+            ps.setString(3, name);
+            ps.setInt(4, area);
 
-        ps.executeUpdate();
-        JOptionPane.showMessageDialog(null, "Data is successfully inserted!");
-    } catch (HeadlessException | NumberFormatException | SQLException e) {
-        JOptionPane.showMessageDialog(null, e);
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Data is successfully inserted!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error inserting data: " + e.getMessage());
+        }
+    } catch (HeadlessException | NumberFormatException e) {
+        JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
     }
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
          setVisible(false);
                home object=new home();
                 object.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
         try{
         String name = Name.getText();
         String sql = "DELETE FROM category WHERE name=?";
@@ -316,18 +315,15 @@ public class AddCategory extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void statussActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_statussActionPerformed
-        // TODO add your handling code here:
     }//GEN-LAST:event_statussActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
         setVisible(false);
         ShowCategory object=new ShowCategory();
         object.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void NameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NameActionPerformed
-        // TODO add your handling code here:
     }//GEN-LAST:event_NameActionPerformed
 
     /**
